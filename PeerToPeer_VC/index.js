aef0713917c826a994033455f5205443d094d46a
Hugh Day-Williams <32854876+HDWilliams@users.noreply.github.com>
2019-09-16 11:38:04 -0400

    reorganizing directories

--- PeerToPeer_VC/index.js
+++
@@ -1,9 +0,0 @@
-//require necessary modules and set port
-var PORT = process.env.PORT || 5000;
-
-var express = require('express');
-var http = require('http');
-
-var Mongo = require('mongodb');
-var URI = process.env.MONGODB_URI
-
