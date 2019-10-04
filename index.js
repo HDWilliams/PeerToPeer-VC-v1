c061e5c8e21d8afea84ae3f2f17b1167e64af650
HDWilliams <hughdaywilliams@gmail.com>
2019-10-03 23:19:22 -0700

    Added PeerJs, made a route to test peer id

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 5000;
 const express = require('express');
 const http = require('http');
 
+//import peerjs library
+import Peer from 'peerjs';
+
 const Mongo = require('mongodb');
 const URI = process.env.MONGODB_URI;
 
@@ -13,6 +16,13 @@ const app = express();
 app.get('/', (req, res)=>{
 	res.status(200);
 	res.send("This is a placeholder!");
+})
+
+app.get('newchat', (req, res)=>{
+	const peer = new Peer();
+	res.status(200);
+	res.send(`Your Id is ${peer.id}`);
+
 })
 
 app.listen(PORT, ()=> console.log(`I'm listening on port ${PORT}`));
