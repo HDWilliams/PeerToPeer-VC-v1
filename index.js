f18f1d3553819a5fa9f1530f5075aa9cb6fb7c23
HDWilliams <hughdaywilliams@gmail.com>
2019-10-03 23:33:14 -0700

    debugging in process

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const http = require('http');
 
 //import peerjs library
-//import Peer from 'peerjs';
+import Peer from 'peerjs';
 
 const Mongo = require('mongodb');
 const URI = process.env.MONGODB_URI;
