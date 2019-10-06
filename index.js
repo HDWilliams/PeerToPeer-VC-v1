134b1274145c47eb70f4011476b429217df4eb43
HDWilliams <hughdaywilliams@gmail.com>
2019-10-06 00:57:37 -0700

    added some basic mongo functionality, need to test connectivity with db

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const PORT = process.env.PORT || 5000;
 
 const express = require('express');
 const http = require('http');
-
+const assert = require('assert');
 //import peerjs library
 //const peer = require('peer');
+//const URI = process.env.MONGODB_URI;
+const URI = 'mongodb://<dbuser>:<dbpassword>@ds261616.mlab.com:61616/heroku_xr0pdhrx';
+const MongoClient = require('mongodb').MongoClient;
 
-const Mongo = require('mongodb');
-const URI = process.env.MONGODB_URI;
+//set up db connection on startup
+MongoClient.connect(URI, function(err, client) {
+	assert.equal(null, err);
+	console.log('Connected to DB');
+
+	const db = client.db('heroku_xr0pdhrx');
+})
+//establish variables for db
+const OpenChats = db.collection('OpenChats');
+const Users = db.collection('Users');
+
 
 const app = express();
 
@@ -20,10 +32,14 @@ app.get('/', (req, res)=>{
 	res.send("This is a placeholder!");
 })
 
-app.get('/newchat', (req, res)=>{
-	//const peer = new Peer();
+//return a list of all open chats in the db
+app.get('/getOpenChats', (req, res)=>{
 	res.status(200);
-	res.send(`Your Id is `);
+	OpenChats.find({}).toArray(function(err, chats) {
+		assert.equal(err, null);
+		console.log(`Current chats open: ${chats}`);
+	})
+
 
 })
 
@@ -37,7 +53,13 @@ app.use('/peerjs', peerServer);
 
 peerServer.on('connection', (client) => {
 	console.log('Connection established from ', client);
+	Users.insert({UserID: client.id}, function(err, results) {
+		console.log('client ID added to Users collection');
+	});
 })
 peerServer.on('disconnect', (client) => {
 	console.log('Disconnection from ', client);
+	Users.deleteOne({UserID: client.id}, function(err, results) {
+		console.log('User document removed');
+	});
 })
