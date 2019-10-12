a9e676e2a9f44f1ae9e769c4fe53b239c85ff075
Rohun Dhar <rohundhar@sonos.com>
2019-10-12 17:44:46 -0400

    comment out build errors

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
   
 const SERVER = app.listen(PORT, ()=> console.log(`I'm listening on port ${PORT}`));
 
